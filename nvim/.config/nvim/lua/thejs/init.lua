9f0b216abb2efa03db10f15449d3d08215b45e95
Jessy Simeon <jessys34@hotmail.fr>
2024-07-28 11:16:02 +0200

    Fix diagnostic virtual text

--- nvim/.config/nvim/lua/thejs/init.lua
+++ nvim/.config/nvim/lua/thejs/init.lua
@@ -3,7 +3,7 @@ require("thejs.set")
 require("thejs.lazy")
 
 vim.diagnostic.config({
-    virtual_text = false,
+    virtual_text = true,
     update_in_insert = true,
     float = true,
     signs = true,
