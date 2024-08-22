9eb75393052bb3add843405d2d3daabeb5aaa09a
Jessy Simeon <jessys34@hotmail.fr>
2024-08-22 16:54:50 +0200

    Now toggle harpoon file

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -13,7 +13,7 @@ return {
             local mark = require("harpoon.mark")
             local ui = require("harpoon.ui")
 
-            vim.keymap.set("n", "<D-:>", mark.add_file)
+            vim.keymap.set("n", "<D-:>", mark.toggle_file)
             vim.keymap.set("n", "<D-=>", ui.toggle_quick_menu)
             vim.keymap.set("n", "<D-,>", ui.nav_prev)
             vim.keymap.set("n", "<D-;>", ui.nav_next)
