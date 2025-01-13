a708cbd23d6bddbc3bd3d8b5463aa28d30ba1fb4
Jessy Simeon <jessys34@hotmail.fr>
2025-01-13 11:45:07 +0100

    nvim: add keymap to toggle undotree

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,5 +1,9 @@
 return {
     {
         "mbbill/undotree",
+        config = function()
+            vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+            vim.g.undotree_SetFocusWhenToggle = true
+        end,
     },
 }
