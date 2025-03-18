ea4799af6e8bc4dafab0a9e96ac1e6ba8c950bd6
Jessy Simeon <jessys34@hotmail.fr>
2025-03-18 16:30:20 +0900

    tmux - fix tmux not calling script at startup. Remove unused lines.

--- tmux/.tmux/scripts/cpu-usage.sh
+++ tmux/.tmux/scripts/cpu-usage.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 # Sums all percentages and print the result.
-# Not very accurate, quite slow.
+# Not accurate at all, can go above 200% (what?).
+# Quite slow.
 ps -A -o %cpu | awk '{s+=$1} END {print s "%"}'
